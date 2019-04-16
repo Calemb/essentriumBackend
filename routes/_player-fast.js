c938e1ce437ef018ca18f91411f37f6b033ea09a
sculpt0r <calemb@gmail.com>
2019-04-16 05:40:54 +0200

    Remove labels from player data for now

--- routes/_player-fast.js
+++ routes/_player-fast.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+//ASSUME [DESIGN] imporatant is your actions, than choosen class
+//in fact - more work you put in certain activities -> you got 'tag' for activities and bonuses for that!
+//maybe make some time bonuses for regular activities
 router.get('/', function (req, res, next) {
     console.log(res.__dirname)
     res.json({
-        l_name: 'Name',
-        name: 'ess name'
+        name: 'John Doe'
     }).end()
 });
 
