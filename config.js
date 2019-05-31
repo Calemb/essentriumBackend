7d27c5556e7d0575a20f18f63e14a7f2a07ac52d
sculpt0r <calemb@gmail.com>
2019-05-31 05:30:38 +0200

    Add ip to cors

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ const config = {
   dev: {
     port: 3001,
     socketOrigins: "http://127.0.0.1:3002",
-    appOrigin: ['http://127.0.0.1:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3002']
+    appOrigin: ['http://127.0.0.1:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3002', 'http://127.0.0.1:3003']
   }
 }
 
 
-module.exports = config.prod
+module.exports = config.dev
