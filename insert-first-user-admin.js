86a60b4538fcc5794ab74c816b32fae4bd35946b
sculpt0r <calemb@gmail.com>
2019-08-09 05:32:42 +0200

    Prepare console command to create first admin user

--- insert-first-user-admin.js
+++ insert-first-user-admin.js
@@ -1,41 +1,48 @@
 const store = require('./local_modules/store.js')
 const bcrypt = require('bcrypt')
-var argv = require('yargs')
-    .usage('Usage: $0 -u [mail] -p [password] -n [name]')
-    .demandOption(['u', 'p', 'n'])
-    .argv;
+const yargs = require('yargs')
 
-// take mail & input password
-//WORKING - make this script first entry to db with admin user! run it with parameter to not store password in git!
-//https://www.npmjs.com/package/yargs
+let createPlayer = function (argv) {
+  const email = argv.email
 
-//FEATURE translate to other langs: https://github.com/projectfluent/fluent.js
-const email = argv.u
+  const name = argv.name
+  let hashPass = argv.password
+  const saltRounds = 1;
+ 
+  // bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+  //   // res == true
+  // });
 
-const name = argv.n
-let hashPass = ''
-bcrypt.genSalt(saltRounds, function (err, salt) {
-    bcrypt.hash(argv.p, salt, function (err, hash) {
-        // Store hash in your password DB.
-        hashPass = hash
-    })
-})
-
-const accountData = {
-    email: email,
-    password: hashPass
+  bcrypt.hash(hashPass, saltRounds)
+    .then((hash) => preparePlayerData(hash, email, name))
 }
-const playerData = (id) => {
-    const obj = {
-        name: name,
-        hp: 10,
-        strength: 1.0,
-        toughness: 1.0,
-        willpower: 1.0
-    }
 
-    if (id != '') { obj._id = id }
-    return obj
+const preparePlayerData = function (hash, email, name) {
+  const accountData = {
+    email: email,
+    password: hash
+  }
+
+  console.table(accountData)
+
+  store.connect((db) => { insertFakePlayer(accountData, name, db) })
+}
+
+//WORKING - make this script first entry to db with admin user! run it with parameter to not store password in git!
+// //https://www.npmjs.com/package/yargs
+//FEATURE translate to other langs: https://github.com/projectfluent/fluent.js
+
+const playerData = (id, name) => {
+  const obj = {
+    name: name,
+    hp: 10,
+    strength: 1.0,
+    toughness: 1.0,
+    willpower: 1.0
+  }
+
+  if (id != '') { obj._id = id }
+  return obj
 }
 // const travelData = (id) => {
 //     const obj = {
@@ -46,31 +53,38 @@ const playerData = (id) => {
 //     if (id != '') { obj._id = id }
 //     return obj
 // }
-const insertFakePlayer = (db) => {
-    db.collection('accounts')
-        .insert(accountData,
-            (err, response) => {
-                if (err) { console.log(err) }
-                console.log(response.ops[0])
-                db.collection('players').insert(playerData(response.ops[0]._id))
-                // db.collection('travels').insert(travelData(response.ops[0]._id))
-            })
+const insertFakePlayer = (accountData, name, db) => {
+  db.collection('accounts')
+    .insert(accountData,
+      (err, response) => {
+        if (err) { console.log(err) }
+        console.log(response.ops[0])
+        db.collection('players').insert(playerData(response.ops[0]._id, name))
+        // db.collection('travels').insert(travelData(response.ops[0]._id))
+      })
 }
 
-// const upsertAllPlayers = (db) => {
-//     // db.collection('accounts').update({}, accountData)
-//     // db.collection('accounts').updateOne({}, { email: 'calemb@gmail.com' , password: '1234'})
-//     db.collection('players').updateMany({},
-//         { $set: playerData('') },
-//         (err, result) => {
-//             console.log(result.result)
-//         })
-//     db.collection('travels').updateMany({},
-//         { $set: travelData('') },
-//         (err, result) => {
-//             console.log(result.result)
-//         })
-// }
-store.connect(insertFakePlayer)
-// store.connect(upsertAllPlayers)
+yargs.command({
+  command: 'create',
+  usage: 'Usage: $0 <command> [options]',
+  builder: {
+    email: {
+      describe: 'Account email',
+      demandOption: true,
+      type: String
+    },
+    password: {
+      describe: 'password',
+      demandOption: true,
+      type: String
+    },
+    name: {
+      describe: 'Player name',
+      demandOption: true,
+      type: String
+    }
+  },
+  handler: createPlayer
+})
 
+yargs.parse()
