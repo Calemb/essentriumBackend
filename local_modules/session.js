514f3d1a92276fd568b23115e8f5d15049b5c247
sculpt0r <calemb@gmail.com>
2019-08-13 05:54:47 +0200

    Fix - checking priviliges account

--- local_modules/session.js
+++ local_modules/session.js
@@ -23,7 +23,9 @@ var sessionVerify = function (req, res, next) {
                 if (result) {
                     console.log(chalk.yellow("session check method: " + req.method));
 
-                    if (result.priviliges.includes('preview') && req.method != 'GET') {
+                    if (typeof result.priviliges != 'undefined' &&
+                        result.priviliges.includes('preview') &&
+                        req.method != 'GET') {
                         res.json({ msg: 'PREVIEW ACCOUNT!' })
                     } else {
                         req.body._id = result._id
