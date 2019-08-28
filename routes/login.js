1a1b307fd230cfeffafcef0b2c884baf1b169368
sculpt0r <calemb@gmail.com>
2019-08-28 05:32:59 +0200

    Use config page to redirect after login

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const bcrypt = require('bcrypt')
 
 var sessionCfg = require('../local_modules/session')
+const config = require('../config')
 /* GET users listing. */
 var account = require('../store/account')
 
@@ -15,12 +16,15 @@ router.post('/', sessionCfg.plain, function (req, res, next) {
   var findedAccout = account.find(req.body.email,
     req.body.password,
     (err, result) => {
+      if (err) {
+        console.log(err)
+      }
       console.log("Result is: " + result)
       if (result) {
         sess.pass = req.body.password
         sess.email = result.email //verify with db
         res.json({
-          location: 'game'
+          location: config.pageAfterLogin
         }).end()
       }
       else {
