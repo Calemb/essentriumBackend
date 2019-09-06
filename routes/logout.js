549d3b7d81b9fc10a4bbdba3eda8c358dcdd036d
sculpt0r <calemb@gmail.com>
2019-09-06 05:27:24 +0200

    Correct logout

--- routes/logout.js
+++ routes/logout.js
@@ -4,10 +4,13 @@ const sessionCfg = require('../local_modules/session')
 const chalk = require('chalk')
 const config = require('../config')
 
-router.post('/', sessionCfg.strict, function (req, res, next) {
+router.post('/', sessionCfg.plain, function (req, res, next) {
     var sess = req.session
-    delete sess.email
-    delete sess.password
+
+    if (sess.email || sess.password) {
+        delete sess.email
+        delete sess.password
+    }
 
     console.log(chalk.red("LOG OUT: " + sess.email))
     res.json({ location: config.pageafterLogout })
