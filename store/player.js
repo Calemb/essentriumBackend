cd1722435e2ab1ae8fbf82fc03f840dabac0d7bc
sculpt0r <calemb@gmail.com>
2019-09-06 05:28:04 +0200

    Small corrects

--- store/player.js
+++ store/player.js
@@ -1,10 +1,17 @@
 const store = require('../local_modules/store')
 
 const player = {};
+player.playersList = function () {
+  return new Promise(resolve => {
+    store.db.collection('players').find({}).toArray((err, result) =>
+      resolve({ err, result })
+    )
+  })
+}
 player.find = function (id, next) {
-    store.db.collection('players')
-        .findOne({
-            _id: id
-        }, next)
+  store.db.collection('players')
+    .findOne({
+      _id: id
+    }, next)
 }
 module.exports = player
