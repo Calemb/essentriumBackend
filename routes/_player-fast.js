0a25d14794961c1d5df07681245ecebba0a2e5ba
sculpt0r <calemb@gmail.com>
2019-09-20 05:40:40 +0200

    _player-fast: merge view with error as response

--- routes/_player-fast.js
+++ routes/_player-fast.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-
 const gameplay = require('../LogicControllers/player')
 
-//ASSUME [DESIGN] imporatant is your actions, than choosen class
-//in fact - more work you put in certain activities -> you got 'tag' for activities and bonuses for that!
-//maybe make some time bonuses for regular activities
+const playerSmallView = { name: '', hp: '' }
+
 router.get('/', (req, res, next) => {
     gameplay.GetLoggedPlayerStats(req).then(({ err, result }) => {
-        if (err) { res.json(err) }
+        if (err) {
+            //merge error with view
+            res.json({ ...err, ...playerSmallView })
+        }
 
-        res.json({
-            name: result.name,
-            hp: result.hp
-        })
+        //fill data before send
+        playerSmallView.name = result.name
+        playerSmallView.hp = result.hp
+
+        res.json(playerSmallView)
     })
 });
 
 module.exports = router;
+//ASSUME [DESIGN] imporatant is your actions, more than choosen class
+//in fact - more work you put in certain activities -> you got 'tag' for activities and bonuses for that!
+//maybe make some time bonuses for regular activities
